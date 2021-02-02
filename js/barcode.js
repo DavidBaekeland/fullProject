e6cfdf8a670aea432c696b16158b0327522a5058
David Baekeland <david.baekeland@student.ehb.be>
2021-02-02 20:17:23 +0100

    barcode.js

---
+++ js/barcode.js
@@ -0,0 +1,26 @@
+let debarcode = document.forms["form"]["debarcode"];
+
+let debarcode_error = document.getElementById("debarcode_error");
+
+let winner = { win: "abcedfghijkl" };
+
+debarcode.addEventListener("textInput", debarcode_Verify);
+
+function validated() {
+  
+
+  if (debarcode.value.length < 12) {
+    debarcode.style.border = "1px solid red";
+    debarcode_error.style.display = "block";
+    debarcode.focus();
+    return false;
+  }
+}
+
+function debarcode_Verify() {
+  if (debarcode.value.length >= 11) {
+    debarcode.style.border = "1px solid silver";
+    debarcode_error.style.display = "none";
+    return true;
+  }
+}
