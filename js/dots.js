88ffffc76f27842a334839b0dbffa8f34c973c50
David Baekeland <david.baekeland@student.ehb.be>
2021-02-02 20:10:26 +0100

    dots.js

---
+++ js/dots.js
@@ -0,0 +1,38 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  console.log(slides);
+  console.log(n);
+  
+  // if (n > slides.length) {
+  //   slideIndex = 1;
+  // }
+  // if (n < 1) {
+  //   slideIndex = slides.length;
+  // }
+  // console.log(n);
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  // console.log(slideIndex - 1, slideIndex - 1);
+  slides[slideIndex - 1].style.display = "block";
+  // console.log(dots[slideIndex - 1]);
+  
+  dots[slideIndex - 1].className += " active";
+}
